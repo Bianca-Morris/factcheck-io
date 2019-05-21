84aea11ba1c018627a9e6713127782fcf0875f2f
Bianca Morris <bmorris@pih.org>
2019-05-21 18:13:26 -0400

    react app scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'index'),
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [{
+      test: /\.js$/, 
+      exclude: /node_modules/,
+      loader: ['babel-loader']
+    },
+    {
+      test: /(\.css)$/,
+      loader: ['style-loader', 'css-loader']
+    }]
+  }
+}
